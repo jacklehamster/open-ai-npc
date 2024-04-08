88d19c4c31d49ffd8d075cdb992e35926cca1af4
Jack Le Hamster <jacklehamster@gmail.com>
2024-04-07 21:59:44 -0700

    Auto-commit: 2024-04-07 21:59:44
    Files:
    src/index.ts
    
    https://github.com/jacklehamster/autocommit

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,60 @@ app.get("/index.html", async (req, res) => {
 
 app.get("/", (req, res) => {
   res.send(`
+  <html>
+  <head>
+    OpenAI NPC Dialog Generator
+    <style>
+/* CSS */
+.button-15 {
+  text-decoration: none;
+  background-image: linear-gradient(#42A1EC, #0070C9);
+  border: 1px solid #0077CC;
+  border-radius: 4px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  direction: ltr;
+  display: block;
+  font-family: "SF Pro Text","SF Pro Icons","AOS Icons","Helvetica Neue",Helvetica,Arial,sans-serif;
+  font-size: 17px;
+  font-weight: 400;
+  letter-spacing: -.022em;
+  line-height: 1.47059;
+  min-width: 30px;
+  overflow: visible;
+  padding: 4px 15px;
+  text-align: center;
+  vertical-align: baseline;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+}
+
+.button-15:disabled {
+  cursor: default;
+  opacity: .3;
+}
+
+.button-15:hover {
+  background-image: linear-gradient(#51A9EE, #147BCD);
+  border-color: #1482D0;
+  text-decoration: underline;
+}
+
+.button-15:active {
+  background-image: linear-gradient(#3D94D9, #0067B9);
+  border-color: #006DBC;
+  outline: none;
+}
+
+.button-15:focus {
+  box-shadow: rgba(131, 192, 253, 0.5) 0 0 0 3px;
+  outline: none;
+}
+    </style>
+  </head>
   <body>
   <h1>NPC Dialog Generator using OpenAI</h1>
   <h4><b>Author:</b> <a href="https://github.com/jacklehamster">jacklehamster</a></h4>
@@ -44,9 +98,21 @@ app.get("/", (req, res) => {
   <div>
     You can check out the NPC dialog demo, which uses this API. Click here:
     <br>
-    <h2>
-      <a href="/example">NPC Dialog Demo</a>
-    </h2>
+    <div style="display: flex; gap: 50px; margin: 40px; text-align: center">
+      <div>
+        <h1>DEMO</h1>
+        <a href="example" class="button-15" role="button">NPC Dialog Demo</a>
+      </div>
+      <div>
+        <h1>Test the API</h1>
+        <a id="api-link" href="api" class="button-15" role="button">/api</a>
+      </div>
+    </div>
+    <hr>
+
+    <script>
+      document.getElementById("api-link").textContent = location.protocol + location.hostname + "/api";
+    </script>
   </div>
   <div>
     Source on Github:
@@ -58,6 +124,7 @@ app.get("/", (req, res) => {
   </div>
 
   </body>
+  </html>
   `);
 });
 
