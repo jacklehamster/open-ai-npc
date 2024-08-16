a47aca5990911a387524b9f138506c761fc5220a
Jack Le Hamster <jacklehamster@gmail.com>
2024-08-16 04:13:32 -0700

    Add routes for editing data via Github.

--- example/index.ts
+++ example/index.ts
@@ -4,6 +4,7 @@ import serveStatic from "serve-static-bun";
 const app = new Bao();
 
 app.get("/*any", serveStatic("/", { middlewareMode: "bao" }));
+app.get("/*any", serveStatic("html", { middlewareMode: "bao" }));
 
 const server = app.listen({ port: 3000 });
 console.log(`Listening on http://localhost:${server.port}`);
