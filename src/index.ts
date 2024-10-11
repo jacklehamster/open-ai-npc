d27bd748ce18ac522388f9d2d46c96057159b765
Jack Le Hamster <jacklehamster@gmail.com>
2024-10-11 12:24:33 -0700

    I'm on fire! Made some updates and crushed a few bugs. - Updated dok-db-manager from version 1.0.15 to 1.0.18 in package.json. Version control? Nailed it! - Added 'OPTIONS' to the 'Access-Control-Allow-Methods' header in src/index.ts. Allowing multiple methods now, making it more flexible for future needs.
    Commit message: "Fixed bug preventing certain HTTP methods and updated dok-db-manager to the latest version. Yay for progress!"
    
    If you were a bit more specific about the bug you fixed, we would've celebrated your victory with a cake joke. Keep it up!
    
    Files:
    M  bun.lockb
    M  package.json
    M  src/index.ts
    
    Committed using auto-commit: Fri Oct 11 2024 12:24:33 GMT-0700 (Pacific Daylight Time)
    Autocommit project: https://www.npmjs.com/package/@dobuki/autocommit
    All the lame jokes were generated by AI.

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', "true");
   next();
