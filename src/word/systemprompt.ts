0ab702cbcda0a9760148431f387d176b08734360
Jack Le Hamster <jacklehamster@gmail.com>
2024-11-08 17:12:18 -0800

    definitions.

--- src/word/systemprompt.ts
+++ src/word/systemprompt.ts
@@ -5,6 +5,14 @@ Respond with only the definition including common use.
 Do not, under any circumstance, use the word in the definition. This is meant as a hint for a word guessing game.
 Make sure the definition doesn't make the word too obvious.
 Double-check to make sure the word or any derrivative is not included in the definition.
-Then show "Example" with a funny sentence using that word, but replace the word in the sentence with ____.
+Then show 3 distinct "Example" with a funny sentence using that word, but replace the word in the sentence with ____.
 
+The format should be as follow:
+Definition: [definition]
+---
+Example1: [sentence1]
+---
+Example2: [sentence2]
+---
+Example3: [sentence3]
 `;
